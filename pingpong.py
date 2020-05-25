d6bb3b97fc11829abb0b798f662703e543532cd1
PostalChief <61651010+PostalChief@users.noreply.github.com>
2020-05-25 13:46:20 +0300

    Update pingpong.py

--- pingpong.py
+++ pingpong.py
@@ -1,6 +1,4 @@
-Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from tkinter import *
+from tkinter import *
 import random
 import time
 class Ball:
